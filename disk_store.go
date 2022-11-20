0ae4fab6f8ec0b213c3eafd7224f21829ec47cab
avi <hi@avi.im>
2022-11-20 17:17:14 +0530

    add some comments

--- disk_store.go
+++ disk_store.go
@@ -9,19 +9,87 @@ import (
 	"time"
 )
 
+// defaultWhence helps us with `file.Seek` method to move our cursor to certain byte offset for read
+// or write operations. The method takes two parameters file.Seek(offset, whence).
+// The offset says the byte offset and whence says the direction:
+//
+// whence 0 - beginning of the file
+// whence 1 - current cursor position
+// whence 2 - end of the file
+//
+// read more about it here:
+// https://pkg.go.dev/os#File.Seek
 const defaultWhence = 0
 
+// DiskStore is a Log-Structured Hash Table as described in the BitCask paper. We
+// keep appending the data to a file, like a log. DiskStorage maintains an in-memory
+// hash table called KeyDir, which keeps the row's location on the disk.
+//
+// The idea is simple yet brilliant:
+//   - Write the record to the disk
+//   - Update the internal hash table to point to that byte offset
+//   - Whenever we get a read request, check the internal hash table for the address,
+//     fetch that and return
+//
+// KeyDir does not store values, only their locations.
+//
+// The above approach solves a lot of problems:
+//   - Writes are insanely fast since you are just appending to the file
+//   - Reads are insanely fast since you do only one disk seek. In B-Tree backed
+//     storage, there could be 2-3 disk seeks
+//
+// However, there are drawbacks too:
+//   - We need to maintain an in-memory hash table KeyDir. A database with a large
+//     number of keys would require more RAM
+//   - Since we need to build the KeyDir at initialisation, it will affect the startup
+//     time too
+//   - Deleted keys need to be purged from the file to reduce the file size
+//
+// Read the paper for more details: https://riak.com/assets/bitcask-intro.pdf
+//
+// DiskStore provides two simple operations to get and set key value pairs. Both key
+// and value need to be of string type, and all the data is persisted to disk.
+// During startup, DiskStorage loads all the existing KV pair metadata, and it will
+// throw an error if the file is invalid or corrupt.
+//
+// Note that if the database file is large, the initialisation will take time
+// accordingly. The initialisation is also a blocking operation; till it is completed,
+// we cannot use the database.
+//
+// Typical usage example:
+//
+//		store, _ := NewDiskStore("books.db")
+//	   	store.Set("othello", "shakespeare")
+//	   	author := store.Get("othello")
 type DiskStore struct {
-	file          *os.File
+	// file object pointing the file_name
+	file *os.File
+	// current cursor position in the file where the data can be written
 	writePosition int
-	keyDir        map[string]KeyEntry
+	// keyDir is a map of key and KeyEntry being the value. KeyEntry contains the position
+	// of the byte offset in the file where the value exists. key_dir map acts as in-memory
+	// index to fetch the values quickly from the disk
+	keyDir map[string]KeyEntry
+}
+
+func isFileExists(fileName string) bool {
+	// https://stackoverflow.com/a/12518877
+	if _, err := os.Stat(fileName); err == nil || errors.Is(err, fs.ErrExist) {
+		return true
+	}
+	return false
 }
 
 func NewDiskStore(fileName string) (*DiskStore, error) {
 	ds := &DiskStore{keyDir: make(map[string]KeyEntry)}
-	if _, err := os.Stat(fileName); err == nil || errors.Is(err, fs.ErrExist) {
+	// if the file exists already, then we will load the key_dir
+	if isFileExists(fileName) {
 		ds.initKeyDir(fileName)
 	}
+	// we open the file in following modes:
+	//	os.O_APPEND - says that the writes are append only.
+	// 	os.O_RDWR - says we can read and write to the file
+	// 	os.O_CREATE - creates the file if it does not exist
 	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		return nil, err
@@ -31,13 +99,24 @@ func NewDiskStore(fileName string) (*DiskStore, error) {
 }
 
 func (d *DiskStore) Get(key string) string {
-	kv, ok := d.keyDir[key]
+	// Get retrieves the value from the disk and returns. If the key does not
+	// exist then it returns an empty string
+	//
+	// How get works?
+	//	1. Check if there is any KeyEntry record for the key in keyDir
+	//	2. Return an empty string if key doesn't exist
+	//	3. If it exists, then read KeyEntry.totalSize bytes starting from the
+	//     KeyEntry.position from the disk
+	//	4. Decode the bytes into valid KV pair and return the value
+	//
+	kEntry, ok := d.keyDir[key]
 	if !ok {
 		return ""
 	}
+	// move the current pointer to the right offset
 	// TODO: handle errors
-	d.file.Seek(int64(kv.position), defaultWhence)
-	data := make([]byte, kv.totalSize)
+	d.file.Seek(int64(kEntry.position), defaultWhence)
+	data := make([]byte, kEntry.totalSize)
 	// TODO: handle errors
 	_, err := io.ReadFull(d.file, data)
 	if err != nil {
@@ -48,14 +127,24 @@ func (d *DiskStore) Get(key string) string {
 }
 
 func (d *DiskStore) Set(key string, value string) {
+	// Set stores the key and value on the disk
+	//
+	// The steps to save a KV to disk is simple:
+	// 1. Encode the KV into bytes
+	// 2. Write the bytes to disk by appending to the file
+	// 3. Update KeyDir with the KeyEntry of this key
 	timestamp := uint32(time.Now().Unix())
 	size, data := encodeKV(timestamp, key, value)
 	d.write(data)
 	d.keyDir[key] = NewKeyEntry(timestamp, uint32(d.writePosition), uint32(size))
+	// update last write position, so that next record can be written from this point
 	d.writePosition += size
 }
 
 func (d *DiskStore) Close() bool {
+	// before we close the file, we need to safely write the contents in the buffers
+	// to the disk. Check documentation of DiskStore.write() to understand
+	// following the operations
 	// TODO: handle errors
 	d.file.Sync()
 	if err := d.file.Close(); err != nil {
@@ -66,16 +155,27 @@ func (d *DiskStore) Close() bool {
 }
 
 func (d *DiskStore) write(data []byte) {
-	// TODO: handle errors
-	_, err := d.file.Write(data)
-	if err != nil {
+	// saving stuff to a file reliably is hard!
+	// if you would like to explore and learn more, then
+	// start from here: https://danluu.com/file-consistency/
+	// and read this too: https://lwn.net/Articles/457667/
+	if _, err := d.file.Write(data); err != nil {
 		panic(err)
 	}
-	// TODO: handle errors
-	d.file.Sync()
+	// calling fsync after every write is important, this assures that our writes
+	// are actually persisted to the disk
+	if err := d.file.Sync(); err != nil {
+		panic(err)
+	}
 }
 
 func (d *DiskStore) initKeyDir(existingFile string) {
+	// we will initialise the keyDir by reading the contents of the file, record by
+	// record. As we read each record, we will also update our keyDir with the
+	// corresponding KeyEntry
+	//
+	// NOTE: this method is a blocking one, if the DB size is yuge then it will take
+	// a lot of time to startup
 	file, _ := os.Open(existingFile)
 	defer file.Close()
 	for {
--- format.go
+++ format.go
@@ -1,12 +1,80 @@
 package caskdb
 
+// format file provides encode/decode functions for serialisation and deserialisation
+// operations
+//
+// format methods are generic and does not have any disk or memory specific code.
+//
+// The disk storage deals with bytes; you cannot just store a string or object without
+// converting it to bytes. The programming languages provide abstractions where you
+// don't have to think about all this when storing things in memory (i.e. RAM).
+// Consider the following example where you are storing stuff in a hash table:
+//
+//    books = {}
+//    books["hamlet"] = "shakespeare"
+//    books["anna karenina"] = "tolstoy"
+//
+// In the above, the language deals with all the complexities:
+//
+//    - allocating space on the RAM so that it can store data of `books`
+//    - whenever you add data to `books`, convert that to bytes and keep it in the memory
+//    - whenever the size of `books` increases, move that to somewhere in the RAM so that
+//      we can add new items
+//
+// Unfortunately, when it comes to disks, we have to do all this by ourselves, write
+// code which can allocate space, convert objects to/from bytes and many other operations.
+//
+// This file has two functions which help us with serialisation of data.
+//
+//    encodeKV - takes the key value pair and encodes them into bytes
+//    decodeKV - takes a bunch of bytes and decodes them into key value pairs
+//
+//**workshop note**
+//
+//For the workshop, the functions will have the following signature:
+//
+//    func encodeKV(timestamp uint32, key string, value string) (int, []byte)
+//    func decodeKV(data []byte) (uint32, string, string)
+
 import "encoding/binary"
 
+// headerSize specifies the total header size. Our key value pair, when stored on disk
+// looks like this:
+//
+//	┌───────────┬──────────┬────────────┬─────┬───────┐
+//	│ timestamp │ key_size │ value_size │ key │ value │
+//	└───────────┴──────────┴────────────┴─────┴───────┘
+//
+// This is analogous to a typical database's row (or a record). The total length of
+// the row is variable, depending on the contents of the key and value.
+//
+// The first three fields form the header:
+//
+//	┌───────────────┬──────────────┬────────────────┐
+//	│ timestamp(4B) │ key_size(4B) │ value_size(4B) │
+//	└───────────────┴──────────────┴────────────────┘
+//
+// These three fields store unsigned integers of size 4 bytes, giving our header a
+// fixed length of 12 bytes. Timestamp field stores the time the record we
+// inserted in unix epoch seconds. Key size and value size fields store the length of
+// bytes occupied by the key and value. The maximum integer
+// stored by 4 bytes is 4,294,967,295 (2 ** 32 - 1), roughly ~4.2GB. So, the size of
+// each key or value cannot exceed this. Theoretically, a single row can be as large
+// as ~8.4GB.
 const headerSize = 12
 
+// KeyEntry keeps the metadata about the KV, specially the position of
+// the byte offset in the file. Whenever we insert/update a key, we create a new
+// KeyEntry object and insert that into keyDir.
 type KeyEntry struct {
+	// Timestamp at which we wrote the KV pair to the disk. The value
+	// is current time in seconds since the epoch.
 	timestamp uint32
-	position  uint32
+	// The position is the byte offset in the file where the data
+	// exists
+	position uint32
+	// Total size of bytes of the value. We use this value to know
+	// how many bytes we need to read from the file
 	totalSize uint32
 }
 
